fb8cd75c362aff785a7811c2f50874115ae1eb2e
bt-ech <mgaur5437@gmail.com>
2025-09-28 16:52:30 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+  "login": "bt-ech",
+  "id": 153829578,
+  "node_id": "U_kgDOCStAyg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/153829578?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/bt-ech",
+  "html_url": "https://github.com/bt-ech",
+  "followers_url": "https://api.github.com/users/bt-ech/followers",
+  "following_url": "https://api.github.com/users/bt-ech/following{/other_user}",
+  "gists_url": "https://api.github.com/users/bt-ech/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/bt-ech/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/bt-ech/subscriptions",
+  "organizations_url": "https://api.github.com/users/bt-ech/orgs",
+  "repos_url": "https://api.github.com/users/bt-ech/repos",
+  "events_url": "https://api.github.com/users/bt-ech/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/bt-ech/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Madhav Gaur",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": "MadhavGaur_1234",
+  "public_repos": 11,
+  "public_gists": 2,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2023-12-14T12:37:18Z",
+  "updated_at": "2025-09-28T11:06:07Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -18,6 +54,10 @@ app.get('/login' , (req , res) =>{
 
 app.get('/youtube' , (req , res) =>{
     res.send('<h2>Chai aur Code</h2>');
+})
+
+app.get('/github' , (req , res) => {
+  res.json(githubData);
 })
 
 app.listen(process.env.PORT, () => {
